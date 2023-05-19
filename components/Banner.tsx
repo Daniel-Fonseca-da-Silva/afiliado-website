267f626c868364398706c31c292d09462d6ce895
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-19 13:33:06 -0300

    feat: Fixed Banner to mobile and tablet

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <section
       id="home"
-      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 "
+      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4 xs:items-center sm:items-center md:items-center lg:items-start lgl:items-start xl:items-start"
     >
       <motion.p
         initial={{ y: 10, opacity: 0 }}
@@ -18,7 +18,7 @@ const Banner = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.2 }}
-        className="text-8xl -mt-10 font-titleFont font-bold tracking-wide text-textOrangeStrong"
+        className="text-8xl -mt-14 font-titleFont font-bold tracking-wide bg-gradient-to-r from-btnColor1 to-btnColor2 text-transparent bg-clip-text xs:pt-7 sm:pt-7 md:pt-7 lg:pt-3 lgl:pt-3 xl:pt-3"
       >
         CVL
       </motion.h1>
@@ -26,7 +26,7 @@ const Banner = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.3 }}
-        className="text-4xl font-bodyFont font-bold tracking-wide -mt-5"
+        className="text-4xl font-bodyFont font-bold tracking-wide -mt-5 xs:px-5 sm:px-5 md:px-0 lg:px-0"
       >
         Aprenda na prática a comprar, <br /> vender e{" "}
         <span className="text-textOrangeLight">
@@ -38,7 +38,7 @@ const Banner = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.4 }}
-        className="text-3xl font-bodyFont tracking-wide font-light -mt-5"
+        className="text-3xl font-bodyFont tracking-wide font-light -mt-5 xs:px-5 sm:px-5 md:px-0 lg:px-0"
       >
         Agora você poderá ter acesso ao método <br /> desenvolvido após{" "}
         <span className="text-textOrangeLight">mais de uma década</span> <br />{" "}
@@ -49,7 +49,7 @@ const Banner = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
-        className="w-2/5 h-14 text-sm font-titleFont  rounded-md text-textGreen tracking-wide bg-gradient-to-r from-btnColor1 to-btnColor2 hover:from-btnColor2 hover:to-btnColor1 font-bold py-2 px-4 animate-bounce border border-blue-500"
+        className="w-2/5 h-14 text-sm font-titleFont rounded-md text-textGreen tracking-wide bg-gradient-to-r from-btnColor1 to-btnColor2 hover:from-btnColor2 hover:to-btnColor1 font-bold py-2 px-4 animate-bounce border shadow-md shadow-btnColor1"
       >
         QUERO LUCRAR COM CRIPTO
       </motion.button>
