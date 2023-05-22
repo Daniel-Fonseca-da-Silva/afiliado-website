755e8ec3f7fa2f85687ff289d076bd263af34ca9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-22 01:55:50 -0300

    feat: Fixed Contact to mobile and tablet

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,25 +1,26 @@
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
-    <section
-      id="contact"
-      className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
-    >
-      <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide">
-        04. What&apos;s next?
-      </p>
-      <h2 className="font-titleFont text-5xl font-semibold">Contact Now</h2>
-      <p className="max-w-[600px] text-center text-textDark">
-        Dafon has a proven track record of delivering exceptional web solutions
-        tailored to the unique needs of our clients. With our expertise in web
-        development, user experience design, and advanced technologies, we have
-        successfully transformed numerous businesses online presence, propelling
-        them to new heights of success.
-      </p>
-      <a href="mailto:daniel@dafon.dev">
-        <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
-          Speak With Us
-        </button>
-      </a>
+    <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4 bg-gradient-to-r from-btnColor1 to-btnColor2 transition-300 shadow-2xl">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.7 }}
+        className="w-10/12 my-10"
+      >
+        <div className="flex flex-col items-center">
+          <h2 className="xs:text-lg sm:text-lg md:text-4xl lg:text-4xl font-titleFont font-bold tracking-wide text-center uppercase">
+            Ainda em dúvida sobre o MÉTODO?
+          </h2>
+          <p className="xs:text-base sm:text-base md:text-lg lg:text-xl font-bodyFont tracking-wider py-6">
+            Nossa equipe está disponível para te ajudar.
+          </p>
+          <button className="w-2/5 h-14 text-sm font-titleFont rounded-md text-textGreen tracking-wide bg-gradient-to-r from-btnGreen to-btnGreen hover:from-btnColor2 hover:to-btnfrom-btnGreen font-bold py-2 px-4 animate-bounce border shadow-md shadow-btnGreen uppercase xs:text-sm xs:h-20 xs:mt-5">
+            quero começar agora
+          </button>
+        </div>
+      </motion.div>
     </section>
   );
 };
